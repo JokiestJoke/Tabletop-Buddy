e7f9bc9c45132d09d21cee447837e1967ce43a20
Mark <mdoghram0@gmail.com>
2024-06-18 14:34:14 -0400

    initializing client side application react-bootstrap

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function App() {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h2>test</h2>
+        </Col>
+        <Col>
+          <h2>test</h2>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
