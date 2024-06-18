540f16d72607b73553c1537dc6fa06f762e2a965
Mark <mdoghram0@gmail.com>
2024-06-18 15:00:17 -0400

    began work on MainLayout file, preparing formatting for headers & footers

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-function App() {
+const App = () => {
   return (
     <Container>
       <Row>
@@ -13,9 +13,12 @@ function App() {
         <Col>
           <h2>test</h2>
         </Col>
+        <Col>
+          <h1>test</h1>
+        </Col>
       </Row>
     </Container>
   );
-}
+};
 
 export default App;
