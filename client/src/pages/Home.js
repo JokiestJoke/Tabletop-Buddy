adb9b6a15573924eb57f2c1102bbab711dc63884
Mark Doghramji <JokiestJoke@gmail.com>
2024-08-06 15:03:59 -0400

    fixed issues with bases container and began laying out an home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,38 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainLayout from '../Layouts/MainLayout';
+import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row';
-//import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import ContentCard from '../components/ContentCard';
+import '../scss/_home.scss'
+import dicey from '../utils/Dicey_Mascot.png'
 import DnDPartyImage from '../utils/images/DnD_Party_400x400.png'
 import Warhammer40kGoldenThrone from '../utils/images/Warhammer_Golden_Throne_400_x_400.png'
-import MonsterCreatorImage from '../utils/images/Monster_Creator_400_x_400.png'
-import WrathAndGloryImage from '../utils/images/Wrath_and_Glory_400_x_400.png'
 
 /**
  * The Home component serves as the landing page of the application.
  */
 const Home = () => {
+
+  const { user } = useState(false);
+  
   return (
     <MainLayout>
       <Container className='text-center home'>
-
-        <Row className='d-flex justify-content-sm-around align-items-sm-center flex-sm-wrap'>
-          <ContentCard
-            mediaPathway={DnDPartyImage}
-            cardTitle={'Dungeon & Dragons'}
-            cardText={'Enter a world of fantasy today with the help of our beginner friendly tools.'}
-            buttonText={'Let\'s Begin'}
-          />
-
-          <ContentCard 
-            mediaPathway={Warhammer40kGoldenThrone}
-            cardTitle={'Warhammer 40,000'}
-            cardText={'Jump in to the grim dark future of Warhammer 40,000 with the help of our beginner friendly tools.'}
-            buttonText={'Let\'s Begin'}
-          />
-
-        </Row>
+        {console.log(user)}
+        
+        <h1>Tabletop Buddy</h1>
+        
+        {user ? (
+          <Row className='d-flex justify-content-sm-around align-items-sm-center flex-sm-wrap'>
+            <ContentCard
+              mediaPathway={DnDPartyImage}
+              cardTitle={'Dungeon & Dragons'}
+              cardText={'Enter a world of fantasy today with the help of our beginner friendly tools.'}
+              buttonText={'Let\'s Begin'}
+            />
+  
+            <ContentCard 
+              mediaPathway={Warhammer40kGoldenThrone}
+              cardTitle={'Warhammer 40,000'}
+              cardText={'Jump in to the grim dark future of Warhammer 40,000 with the help of our beginner friendly tools.'}
+              buttonText={'Let\'s Begin'}
+            />
+  
+          </Row>
+        ) : (
+          <>
+            <Col className='d-flex justify-content-center'>
+              <Image src={dicey} alt='Dicey the mascot'/>
+            </Col>
+            <Col>
+              <p>Welcome to Tabletop Buddy, the platform designed for tabletop enthusiasts of all skills levels </p>
+              <p><Link to='/login'>Log in</Link> to begin working on some proofs!</p>
+              <p>Don't have an account? <Link to="/signup">Sign up</Link> now!</p>
+            </Col>
+          </>
+        )
+        }
       </Container>
     </MainLayout>
   );
