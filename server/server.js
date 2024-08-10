e33ae56dd6143db271f7d6230d738060ce63642a
Mark Doghramji <JokiestJoke@gmail.com>
2024-08-10 01:29:25 -0400

    edited gitignore, installed further dependencies

--- server/server.js
+++ server/server.js
@@ -1,13 +1,12 @@
 import express from 'express'
 import app from './app.js';
 
-//const app = express();
-const port = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
   res.send('Welcome to the server!');
 });
 
-   app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
