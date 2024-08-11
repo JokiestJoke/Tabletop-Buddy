09cf77d62756f8de33b3c451b41b8df2439e6a94
Mark Doghramji <JokiestJoke@gmail.com>
2024-08-11 10:39:20 -0400

    ..

--- server/tests/db.config.test.js
+++ server/tests/db.config.test.js
@@ -4,4 +4,22 @@ test('DB_CONFIG_HOST_IS_NOT_NULL',
   () => {
     expect(DB_CONFIG.HOST).not.toBeNull();
   }
-)
+);
+
+test('DB_CONFIG_USER_IS_NOT_NULL', 
+  () => {
+    expect(DB_CONFIG.USER).not.toBeNull();
+  }
+);
+
+test('DB_CONFIG_PASSWORD_IS_NOT_UNDEFINED', 
+  () => {
+    expect(DB_CONFIG.PASSWORD).not.toBeUndefined();
+  }
+);
+
+test('DB_CONFIG_NAME_IS_NOT_NULL', 
+  () => {
+    expect(DB_CONFIG.USER).not.toBeNull();
+  }
+);
